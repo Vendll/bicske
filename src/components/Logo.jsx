8be3a2c6a378b7e2f60b52d8b8ab5d9853dba324
Vendll <utto.vendel@icloud.com>
2022-12-19 20:14:18 +0100

    ready to export

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,16 @@
 import Image from 'next/image'
-import logo from '../../public/BICSKE_UJ_t.png'
-import logo_m from '../../public/BICSKE_UJ_m.png'
+import ExportedImage from 'next-image-export-optimizer'
+import logo from '../images/BICSKE_UJ_t.png'
+import logo_m from '../images/BICSKE_UJ_m.png'
 
 export function Logo() {
   return (
     <>
       <div className="hidden sm:block">
-        <Image src={logo} alt="Bicske" width={200} height={80} />
+        <ExportedImage src={logo} alt="Bicske" width={200} height={80} />
       </div>
       <div className="block sm:hidden">
-        <Image src={logo_m} alt="Bicske" width={50} height={50} />
+        <ExportedImage src={logo_m} alt="Bicske" width={50} height={50} />
       </div>
     </>
   )
